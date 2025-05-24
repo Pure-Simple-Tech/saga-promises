463ad44668d3c30293832da0deca6018ae0159a6
Valentin Harapu <aharapu2@yahoo.com>
2025-05-24 16:12:38 +0300

    feat(saga): create state for saga step

--- src/saga.ts
+++ src/saga.ts
@@ -1,4 +1,14 @@
-export function createSaga(): Promise<"done"> {
+interface SagaContext extends Object {}
+
+// TODO -> each rollback function should be called with the context as it was when the "action" step was called
+
+type SagaStep = {
+  action: (req: Request, ctx: SagaContext) => Promise<void>;
+  rollback: (req: Request, ctx: SagaContext, rej: Error) => Promise<void>;
+};
+
+// TODO -> make this a class and instantiate with the "new" operator?
+export function createSaga(steps: Array<SagaStep> = []): Promise<"done"> {
   return new Promise((res, rej) => {
     res("done");
   });
